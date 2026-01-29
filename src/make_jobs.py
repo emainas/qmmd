296c67ac98d1ee305c1b93b7480831b33782f0e2
Eleftherios Mainas (emainas) <emainas@sycamore-login1.sm.unc.edu>
2026-01-29 15:10:36 -0500

    Add dataclasses

--- src/make_jobs.py
+++ src/make_jobs.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass
+
+@dataclass
+class SlurmConfiguration:
+    """Class for spefifying the configuration of a DCDFTBMD run with slurm."""
+    number_of_nodes:
+    number_of_tasks:
+    cpus_per_task:
+    partition:
+    name:
+    time:
+
+
+
 def main():
     pass
 
